a8d35b397e553cf83fe4d4e83a883af8d344d0aa
Witold Kupś <witold.kups@interlan.pl>
2022-03-18 19:13:43 +0100

    Extract ChartPeriodItemView to separate file

---
+++ lib/symbol/chart_period_item_view.dart
@@ -0,0 +1,43 @@
+
+import 'package:flutter/material.dart';
+
+import '../ui_style.dart';
+import '../xtb/model/chart_period.dart';
+
+class ChartPeriodItemView extends StatelessWidget {
+  final ChartPeriod period;
+  final bool selected;
+  final void Function() onClick;
+
+  const ChartPeriodItemView({
+    Key? key,
+    required this.period,
+    required this.selected,
+    required this.onClick,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    ColorScheme colorScheme = Theme.of(context).colorScheme;
+    return Padding(
+      padding: EdgeInsets.all(UIStyle.contentMarginSmall),
+      child: Material(
+        color: selected ? colorScheme.primary : colorScheme.background,
+        borderRadius: BorderRadius.circular(UIStyle.popupsBorderRadius),
+        elevation: 2,
+        child: InkWell(
+          borderRadius: BorderRadius.circular(UIStyle.popupsBorderRadius),
+          child: Padding(
+            padding: EdgeInsets.all(UIStyle.contentMarginMedium),
+            child: Text(period.tag,
+                style: TextStyle(
+                    color: selected
+                        ? colorScheme.onPrimary
+                        : colorScheme.onBackground)),
+          ),
+          onTap: () => onClick(),
+        ),
+      ),
+    );
+  }
+}
--- lib/symbol/chart_period_selector.dart
+++ lib/symbol/chart_period_selector.dart
@@ -2,43 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:stocker/ui_style.dart';
 import 'package:stocker/xtb/model/chart_period.dart';
 
-class ChartPeriodView extends StatelessWidget {
-  final ChartPeriod period;
-  final bool selected;
-  final void Function() onClick;
-
-  const ChartPeriodView({
-    Key? key,
-    required this.period,
-    required this.selected,
-    required this.onClick,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    ColorScheme colorScheme = Theme.of(context).colorScheme;
-    return Padding(
-      padding: EdgeInsets.all(UIStyle.contentMarginSmall),
-      child: Material(
-        color: selected ? colorScheme.primary : colorScheme.background,
-        borderRadius: BorderRadius.circular(UIStyle.popupsBorderRadius),
-        elevation: 2,
-        child: InkWell(
-          borderRadius: BorderRadius.circular(UIStyle.popupsBorderRadius),
-          child: Padding(
-            padding: EdgeInsets.all(UIStyle.contentMarginMedium),
-            child: Text(period.tag,
-                style: TextStyle(
-                    color: selected
-                        ? colorScheme.onPrimary
-                        : colorScheme.onBackground)),
-          ),
-          onTap: () => onClick(),
-        ),
-      ),
-    );
-  }
-}
+import 'chart_period_item_view.dart';
 
 class ChartPeriodSelector extends StatefulWidget {
   final void Function(ChartPeriod period) periodChanged;
@@ -103,14 +67,14 @@ class _ChartPeriodSelectorState extends State<ChartPeriodSelector> {
   Widget _buildList() {
     if (_extended) {
       return Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-        ...PERIODS.map((period) => ChartPeriodView(
+        ...PERIODS.map((period) => ChartPeriodItemView(
               period: period,
               selected: period == _selected,
               onClick: () => _updateSelection(period),
             ))
       ]);
     } else {
-      return ChartPeriodView(
+      return ChartPeriodItemView(
         period: _selected,
         selected: false,
         onClick: () => _openSelection(),
