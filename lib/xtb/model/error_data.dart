a5dbf048f68727b987fac3704ba2097ed6e16ea9
Witold Kupś <witold.kups@interlan.pl>
2022-03-19 09:21:45 +0100

    Add toString to ErrorData

--- lib/xtb/model/error_data.dart
+++ lib/xtb/model/error_data.dart
@@ -7,10 +7,22 @@ class ErrorData {
     required this.errorDescr,
   });
 
+  Map<String, dynamic> toMap() {
+    return {
+      'errorCode': errorCode,
+      'errorDescr': errorDescr,
+    };
+  }
+
   factory ErrorData.fromMap(Map<String, dynamic> map) {
     return ErrorData(
       errorCode: map['errorCode'] as String,
       errorDescr: map['errorDescr'] as String,
     );
+  }
+
+  @override
+  String toString() {
+    return toMap().toString();
   }
 }
